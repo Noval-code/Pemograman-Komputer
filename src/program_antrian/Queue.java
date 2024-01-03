fb67bec0e775733be07d5657e0aaf15cde1c511b
Noval-code <novalaula486@gmail.com>
2024-01-03 21:14:34 +0700

    new feature done

---
+++ src/program_antrian/Queue.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package program_antrian;
+
+/**
+ *
+ * @author muhammad noval aula
+ */
+public interface Queue {
+    void enqueue(int number);
+    int dequeue();
+    int ukuran();
+}
